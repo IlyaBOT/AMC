a3894019533c155d0ea2a5f8d89ad6a67303ee74
IlyaBOT <36928846+IlyaBOT@users.noreply.github.com>
2021-01-02 12:28:15 +0300

    Upload source files and update programm

---
+++ src/ArduinoMotorController/ArduinoMotorController/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class AMC
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AMC));
+            this.RadioButton1 = new System.Windows.Forms.RadioButton();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.SerialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Label1 = new System.Windows.Forms.Label();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.RadioButton3 = new System.Windows.Forms.RadioButton();
+            this.RadioButton2 = new System.Windows.Forms.RadioButton();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RadioButton1
+            // 
+            this.RadioButton1.AutoSize = true;
+            this.RadioButton1.Checked = true;
+            this.RadioButton1.Location = new System.Drawing.Point(6, 16);
+            this.RadioButton1.Name = "RadioButton1";
+            this.RadioButton1.Size = new System.Drawing.Size(49, 17);
+            this.RadioButton1.TabIndex = 14;
+            this.RadioButton1.TabStop = true;
+            this.RadioButton1.Text = "9600";
+            this.RadioButton1.UseVisualStyleBackColor = true;
+            this.RadioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Terminator Cyr 4", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button1.Location = new System.Drawing.Point(7, 36);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 10;
+            this.Button1.Text = "-";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(13, 17);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(69, 13);
+            this.Label1.TabIndex = 19;
+            this.Label1.Text = "Motor speed:";
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11",
+            "COM12",
+            "COM13",
+            "COM14",
+            "COM15",
+            "COM16",
+            "COM17",
+            "COM18",
+            "COM19",
+            "COM20",
+            "COM21",
+            "COM22",
+            "COM23",
+            "COM24",
+            "COM25"});
+            this.ComboBox1.Location = new System.Drawing.Point(6, 39);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(177, 21);
+            this.ComboBox1.TabIndex = 18;
+            // 
+            // Button4
+            // 
+            this.Button4.Font = new System.Drawing.Font("Yandex-UI-Icons-Private", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button4.Location = new System.Drawing.Point(189, 16);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(88, 44);
+            this.Button4.TabIndex = 17;
+            this.Button4.Text = "Connect";
+            this.Button4.UseVisualStyleBackColor = true;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // RadioButton3
+            // 
+            this.RadioButton3.AutoSize = true;
+            this.RadioButton3.Location = new System.Drawing.Point(122, 16);
+            this.RadioButton3.Name = "RadioButton3";
+            this.RadioButton3.Size = new System.Drawing.Size(61, 17);
+            this.RadioButton3.TabIndex = 16;
+            this.RadioButton3.Text = "115200";
+            this.RadioButton3.UseVisualStyleBackColor = true;
+            this.RadioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // RadioButton2
+            // 
+            this.RadioButton2.AutoSize = true;
+            this.RadioButton2.Location = new System.Drawing.Point(61, 16);
+            this.RadioButton2.Name = "RadioButton2";
+            this.RadioButton2.Size = new System.Drawing.Size(55, 17);
+            this.RadioButton2.TabIndex = 15;
+            this.RadioButton2.Text = "19200";
+            this.RadioButton2.UseVisualStyleBackColor = true;
+            this.RadioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // Button3
+            // 
+            this.Button3.Font = new System.Drawing.Font("Terminator Cyr 4", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Button3.Location = new System.Drawing.Point(88, 36);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(108, 23);
+            this.Button3.TabIndex = 13;
+            this.Button3.Text = "On/Off";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Font = new System.Drawing.Font("Terminator Cyr 4", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Button2.Location = new System.Drawing.Point(202, 36);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(75, 23);
+            this.Button2.TabIndex = 12;
+            this.Button2.Text = "+";
+            this.Button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(88, 16);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(189, 14);
+            this.ProgressBar1.TabIndex = 11;
+            this.ProgressBar1.Value = 50;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Button4);
+            this.groupBox1.Controls.Add(this.RadioButton1);
+            this.groupBox1.Controls.Add(this.RadioButton2);
+            this.groupBox1.Controls.Add(this.RadioButton3);
+            this.groupBox1.Controls.Add(this.ComboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 71);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ProgressBar1);
+            this.groupBox2.Controls.Add(this.Button2);
+            this.groupBox2.Controls.Add(this.Button1);
+            this.groupBox2.Controls.Add(this.Button3);
+            this.groupBox2.Controls.Add(this.Label1);
+            this.groupBox2.Location = new System.Drawing.Point(7, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(283, 66);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Motor control";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel5,
+            this.toolStripStatusLabel6});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 143);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(299, 22);
+            this.statusStrip1.Stretch = false;
+            this.statusStrip1.TabIndex = 22;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(86, 17);
+            this.toolStripStatusLabel2.Text = "Not connected";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(19, 17);
+            this.toolStripStatusLabel4.Text = "    ";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(32, 17);
+            this.toolStripStatusLabel3.Text = "Port:";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(41, 17);
+            this.toolStripStatusLabel5.Text = "COM0";
+            // 
+            // toolStripStatusLabel6
+            // 
+            this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel6.Text = "0";
+            // 
+            // AMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 165);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AMC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arduino Motor Controller";
+            this.Load += new System.EventHandler(this.AMC_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.RadioButton RadioButton1;
+        internal System.Windows.Forms.Button Button1;
+        internal System.IO.Ports.SerialPort SerialPort1;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.ComboBox ComboBox1;
+        internal System.Windows.Forms.Button Button4;
+        internal System.Windows.Forms.RadioButton RadioButton3;
+        internal System.Windows.Forms.RadioButton RadioButton2;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.ProgressBar ProgressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+    }
+}
+
---
+++ src/ArduinoMotorController/ArduinoMotorController/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class AMC : Form
+    {
+        bool isConnected = false; // Статус подключения.
+        bool status = true; // Переменная для центральной кнопки ВКЛ/ВЫКЛ.
+        int speed = 50; // Начальная скорость.
+        int baudRate = 9600; // Скорость передачи при запуске программы.
+        public AMC()
+        {
+            InitializeComponent();
+        }
+
+        private void AMC_Load(object sender, EventArgs e)
+        {
+            string[] portnames = SerialPort.GetPortNames(); // Получаем список COM портов доступных в системе.
+            if (portnames.Length == 0) // Проверяем есть ли доступные.
+            {
+                MessageBox.Show("ERROR: Any COM PORT not found!"); // Выводим сообщение об ошибке.
+            }
+            foreach (string portName in portnames)
+            {
+                ComboBox1.Items.Clear(); // Очищаем все COM-порты.
+                ComboBox1.Items.Add(portName); // Добавляем доступные COM-порты в список.
+                Console.WriteLine(portnames.Length); // Выводим COM-порты в консоль.
+                if (portnames[0] != null){ComboBox1.SelectedItem = portnames[0];} // Непонятная хероборина. Не трогать!
+            }
+            toolStripStatusLabel6.Text = "  Speed:" + "50"; // Изменяем скорость в статус баре.
+            SerialPort1.BaudRate = baudRate; // Устанавливаем скорость передачи.
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // При закрытии программы, отправляем 0 и закрываем порт.
+            if (SerialPort1.IsOpen) SerialPort1.Write("0"); SerialPort1.Close();
+        }
+
+        private void Button2_Click(object sender, EventArgs e) // Button +
+        {
+            if (isConnected){
+                if (ProgressBar1.Value < 100){ // Если скорость меньше 100, то выполняем:
+                    ProgressBar1.Value += 5; // Добавляем 5 к скорости.
+                    speed = ProgressBar1.Value; // Значние переменной ставим равной значению прогресс-бара.
+                    SerialPort1.Write(speed.ToString()); // Отправляем данные через последовательный порт.
+                    toolStripStatusLabel6.Text = "  Speed:" + ProgressBar1.Value; // Обновляем статус-бар.
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e) // Button -
+        {
+            if (isConnected){
+                if (ProgressBar1.Value > 0){ // Если скорость больше 0, то выполняем:
+                    ProgressBar1.Value -= 5; // Отнимаем 5 от скорости.
+                    speed = ProgressBar1.Value; // Значние переменной ставим равной значению прогресс-бара.
+                    SerialPort1.Write(speed.ToString()); // Отправляем данные через последовательный порт.
+                    toolStripStatusLabel6.Text = "  Speed:" + ProgressBar1.Value; // Обновляем статус-бар.
+                }                
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e) // Middle button
+        {
+            if(status == true && isConnected){
+                status = false; // Переменную статуса ставим равной false.
+                speed = 0; // Значние переменной ставим равной 0.
+                ProgressBar1.Value = 0; // Значние прогресс-бара ставим равной 0.
+                SerialPort1.Write("0"); // Отправляем 0 через последовательный порт.
+                toolStripStatusLabel6.Text = "  Speed:" + ProgressBar1.Value; // Обновляем статус-бар.
+            }
+            else if (status == false && isConnected){
+                status = true; // Переменную статуса ставим равной true.
+                speed = 100; // Значние переменной ставим равной 100.
+                ProgressBar1.Value = 100; // Значние прогресс-бара ставим равной 100.
+                SerialPort1.Write("100"); // Отправляем 100 через последовательный порт.
+                toolStripStatusLabel6.Text = "  Speed:" + ProgressBar1.Value; // Обновляем статус-бар.
+            }
+        }      
+        private void connectToArduino() // Connect function
+        {
+            isConnected = true; // Переменную подключения ставим равной true.
+            string selectedPort = ComboBox1.GetItemText(ComboBox1.SelectedItem); // Ставим значние переменной selectedPort равной выбранному COM-порту.
+            SerialPort1.PortName = selectedPort; // Устанавливаем выбранный COM-порт.
+            SerialPort1.Open(); // Подключаемся к выбранному COM-порту.
+            Button4.Text = "Disconnect"; // Надпись на кнопке подключения ставим: Disconnect (Отключиться).
+            toolStripStatusLabel2.Text = "Connected"; // В статус баре пишем: Connected (Подключено).
+            toolStripStatusLabel5.Text = SerialPort1.PortName; // Пишем в статус бар номер подключенного порта.
+        }
+
+        private void disconnectFromArduino() // Disconnect function
+        {
+            isConnected = false; // Переменную подключения ставим равной false.
+            SerialPort1.Write("0"); // Отправляем 0 в последовательный порт.
+            SerialPort1.Close(); // Закрываем последовательный порт.
+            Button4.Text = "Connect"; // Надпись на кнопке подключения ставим: Connect (Подключиться).
+            toolStripStatusLabel2.Text = "Not connected"; // В статус баре пишем: Not connected (Не подключено).
+            toolStripStatusLabel5.Text = "COM0"; // Пишем в статус бар нулевой порт.
+        }
+        private void Button4_Click(object sender, EventArgs e) // Connect button
+        {
+            if (!isConnected){connectToArduino();} // Если не подключено, то выполняем функцию connectToArduino();
+            else{disconnectFromArduino();} // Иначе выполняем функцию disconnectFromArduino();
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e) // Radiobutton 1
+        {
+            int baudRate = 9600; // Устанавливаем переменную скорости передачи 9600.
+            SerialPort1.BaudRate = baudRate; // Устанавливаем скорость передачи равной переменной baudRate.
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e) // Radiobutton 2
+        {
+            int baudRate = 19200; // Устанавливаем переменную скорости передачи 19200.
+            SerialPort1.BaudRate = baudRate; // Устанавливаем скорость передачи равной переменной baudRate.
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e) // Radiobutton 3
+        {
+            int baudRate = 115200; // Устанавливаем переменную скорости передачи 115200.
+            SerialPort1.BaudRate = baudRate; // Устанавливаем скорость передачи равной переменной baudRate.
+        }
+    }
+}
